872146c028838dca639643731561ac6ed89794c4
Erastus Gitau <erastusgitaum9@gmail.com>
2026-01-08 23:14:50 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,20 +4,21 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("KonfirmPay backend is running");
-});
-
 app.post("/ussd", (req, res) => {
-  const { text } = req.body;
-
   res.set("Content-Type", "text/plain");
 
-  if (!text || text === "") {
-    res.send("CON Enter amount to pay:");
-  } else {
-    res.send(`END You entered amount: KES ${text}`);
+  // Africa's Talking sends data as form-urlencoded
+  const text = req.body.text || "";
+
+  if (text === "") {
+    return res.send("CON Enter amount to pay:");
   }
+
+  return res.send(`END You entered amount: KES ${text}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("KonfirmPay backend is running");
 });
 
 const PORT = process.env.PORT || 3000;
