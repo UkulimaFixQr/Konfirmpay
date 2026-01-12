f23994b6b8af09a9e166f0b33a782dcbd1cf0c06
Erastus Gitau <erastusgitaum9@gmail.com>
2026-01-12 17:53:35 +0300

    add admin

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const fetch = (...args) =>
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
 
 /**
  * ====================================================
